35c5aeaeea49113797a0d8ae87f40bd7e24cd169
aadhityakumaran <kumaranpoke@gmail.com>
2024-09-27 01:09:52 +0530

    Templates files set up

--- Donor/routes/index.js
+++ Donor/routes/index.js
@@ -7,13 +7,13 @@ const router = Router();
 router.use(auth);
 
 router.get('/', async (req, res) => {
-    const donor_id = req.query.donor_id;
+    const donor_id = req.token.donor_id;
     const donatables = await getDonatables(donor_id);
     res.render('donatables.ejs', { donatables });
 });
 
 router.get('/profile', async (req, res) => {
-    const donor_id = req.query.donor_id;
+    const donor_id = req.token.donor_id;
     const userData = await getUserData(donor_id);
     res.render('profile.ejs', { userData });
 });

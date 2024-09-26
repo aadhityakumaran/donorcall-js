2ac49d10843eacafa805cb7b0094f8886e34d23f
aadhityakumaran <kumaranpoke@gmail.com>
2024-09-26 20:46:18 +0530

    Database functions

--- Donor/db_connect.js
+++ Donor/db_connect.js
@@ -1,5 +1,6 @@
 import mysql from 'mysql2';
 import config from './db_config';
+import crypto from 'crypto';
 
 const config = {
     host: process.env.DB_HOST || "localhost",
@@ -21,4 +22,46 @@ function connect() {
     });
 }
 
-export default connection;
+async function isValidLogin(donor_id, password) {
+    const query = `SELECT * FROM users WHERE donor_id = '${donor_id}' AND pwd = '${hashPassword(password)}'`;
+    const [rows, fields] = await connection.promise().query(query);
+    return rows.length > 0;
+}
+
+async function registerUser(password, name, phone, blood_group) {
+    const donorId = Math.floor(Math.random() * 9000) + 1000;
+    const query = `INSERT INTO users (donor_id, pwd, phone, name, blood) VALUES ('${donorId}', '${hashPassword(password)}, '${phone}', '${name}', '${blood_group}')`;
+    await connection.promise().query(query);
+}
+
+async function getUserData(donor_id) {
+    const query = `SELECT name, phone, blood, last_don FROM users WHERE donor_id = '${donor_id}'`;
+    const [rows, fields] = await connection.promise().query(query);
+    return rows[0];
+}
+
+async function getBloodGroup(donor_id) {
+    const query = `SELECT blood FROM users WHERE donor_id = '${donor_id}'`;
+    const [rows, fields] = await connection.promise().query(query);
+    return rows[0].blood;
+}
+
+async function getDonatables(donor_id) {
+    const blood_group = await getBloodGroup(donor_id);
+    const query = `SELECT patient_id, name, age, blood, phone, info FROM hospital WHERE blood = '${blood_group}'`;
+    const [rows, fields] = await connection.promise().query(query);
+    return rows;
+}
+
+function hashPassword(password) {
+    const hash = crypto.createHash('sha256');
+    hash.update(password);
+    return hash.digest('hex');
+}
+
+export default {
+    isValidLogin,
+    registerUser,
+    getUserData,
+    getDonatables
+};

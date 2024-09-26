337fdad39f239d5227c102e5f40701b22361e284
aadhityakumaran <kumaranpoke@gmail.com>
2024-09-27 00:03:52 +0530

    Route handlers

--- Donor/routes/index.js
+++ Donor/routes/index.js
@@ -1,21 +1,43 @@
 import { Router } from "express";
+import { isValidLogin, registerUser, getUserData, getDonatables } from './db_connect.js'
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.json({ message: "This is the home route" });
+router.get('/', async (req, res) => {
+    const donor_id = req.query.donor_id;
+    const donatables = await getDonatables(donor_id);
+    res.render('donatables.ejs', { donatables });
 });
 
-router.get('/login', (req, res) => {
+router.get('/login', async (req, res) => {
+    res.render('login.ejs');
+});
+
+router.post('/login', async (req, res) => {
+    const { donor_id, password } = req.body;
+    const isValid = await isValidLogin(donor_id, password);
+    if (isValid) {
+        res.redirect('/');
+    } else {
+        res.json({ message: "Invalid credentials" });
+    }
     res.json({ message: "This is the login route" });
 });
 
 router.get('/signup', (req, res) => {
-    res.json({ message: "This is the signup route" });
+    res.render('signup.ejs');
 });
 
-router.get('/profile', (req, res) => {
-    res.json({ message: "This is the profile route" });
+router.post('/signup', async (req, res) => {
+    const { password, name, phone, blood_group } = req.body;
+    await registerUser(password, name, phone, blood_group);
+    res.redirect('/');
+});
+
+router.get('/profile', async (req, res) => {
+    const donor_id = req.query.donor_id;
+    const userData = await getUserData(donor_id);
+    res.render('profile.ejs', { userData });
 });
 
 router.get('/logout', (req, res) => {

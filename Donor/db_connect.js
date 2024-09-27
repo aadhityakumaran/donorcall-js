684a2c4f204da6db6e882b7e2acdd76ed3ad05d4
aadhityakumaran <kumaranpoke@gmail.com>
2024-09-28 00:03:56 +0530

    DB made sql injection proof

--- Donor/db_connect.js
+++ Donor/db_connect.js
@@ -25,40 +25,41 @@ function connect() {
 }
 
 async function isValidLogin(donor_id, password) {
-    const query = `SELECT * FROM users WHERE donor_id = '${donor_id}' AND pwd = '${hashPassword(password)}'`;
-    const [rows, fields] = await connection.promise().query(query);
+    const query = `SELECT * FROM users WHERE donor_id = ? AND pwd = ?`;
+    const [rows, fields] = await connection.promise().query(query, [donor_id, hashPassword(password)]);
     return rows.length > 0;
 }
 
 async function registerUser(password, name, phone, blood_group) {
     const donorId = Math.floor(Math.random() * 9000) + 1000;
-    const query = `INSERT INTO users (donor_id, pwd, phone, name, blood) VALUES ('${donorId}', '${hashPassword(password)}, '${phone}', '${name}', '${blood_group}')`;
-    await connection.promise().query(query);
+    const query = `INSERT INTO users (donor_id, pwd, phone, name, blood) VALUES (?, ?, ?, ?, ?)`;
+    await connection.promise().query(query, [donorId, hashPassword(password), phone, name, blood_group.substring(0, 2)]);
+    return donorId;
 }
 
 async function getUserData(donor_id) {
-    const query = `SELECT name, phone, blood, last_don FROM users WHERE donor_id = '${donor_id}'`;
-    const [rows, fields] = await connection.promise().query(query);
+    const query = `SELECT name, phone, blood, last_don FROM users WHERE donor_id = ?`;
+    const [rows, fields] = await connection.promise().query(query, [donor_id]);
     return rows[0];
 }
 
 async function getBloodGroup(donor_id) {
-    const query = `SELECT blood FROM users WHERE donor_id = '${donor_id}'`;
-    const [rows, fields] = await connection.promise().query(query);
+    const query = `SELECT blood FROM users WHERE donor_id = ?`;
+    const [rows, fields] = await connection.promise().query(query, [donor_id]);
     return rows[0].blood;
 }
 
 async function getDonatables(donor_id) {
     const blood_group = await getBloodGroup(donor_id);
-    const query = `SELECT patient_id, name, age, blood, phone, info FROM hospital WHERE blood = '${blood_group}'`;
-    const [rows, fields] = await connection.promise().query(query);
+    const query = `SELECT patient_id, name, age, blood, phone, info FROM hospital WHERE blood = ?`;
+    const [rows, fields] = await connection.promise().query(query, [blood_group]);
     return rows;
 }
 
 function hashPassword(password) {
     const hash = crypto.createHash('sha256');
-    hash.update(password);
-    return hash.digest('hex');
+    hash.update(password, 'utf-8');
+    return hash.digest();
 }
 
 export {
